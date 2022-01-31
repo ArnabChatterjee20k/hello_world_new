3c6814c36d8c1acfd7be6c2b2ac2639e86cbf6e1
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-01-31 08:49:36 +0530

    reviewed and corrected all the mistakes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request ,redirect, url_for
 import pickle
 import numpy as np
 
@@ -15,34 +15,36 @@ def home():
     title = 'Home Page'
     return render_template('index.html', title=title)
 
+
+# here the form is present.
 @ app.route('/crop-recommend',methods=['POST','GET'])
 def crop_recommend():
     title = 'Crop Recommendation'
-    return render_template('crop.html', title=title)
-
-
-@ app.route('/crop-predict', methods=['POST','GET'])
-def crop_prediction():
-    title = 'Crop Recommendation'
-
     if request.method == 'POST':
         N = int(request.form['nitrogen'])
         P = int(request.form['phosphorous'])
-        K = int(request.form['pottasium'])
+        K = int(request.form['potasium'])
         ph = float(request.form['ph'])
         T = float(request.form['temprature'])
         H = float(request.form['humidity'])
-        rainfall = float(request.form['raainfall'])
+        rainfall = float(request.form['rainfall'])
 
         data = np.array([[N, P, K, T, H, ph, rainfall]])
         my_prediction = crop_recommendation_model.predict(data)
         final_prediction = my_prediction[0]
         
         final_prediction = "Banana"
-        return render_template('crop-result.html', prediction=final_prediction, title=title)
+        final_prediction = "Done"
+        return redirect(url_for("crop_prediction",prediction=final_prediction)) # redirecting the url of crop-predict using the function
 
-    else:
-        return render_template('try_again.html', title=title)
+    # if request is not post then this template will be sent.
+    return render_template('crop.html', title=title)
+
+
+@ app.route('/crop-predict', methods=['GET'])
+def crop_prediction():
+    title = 'Crop Recommendation'
+    return render_template('crop-result.html', title=title)
 
 
 if __name__ == '__main__':
